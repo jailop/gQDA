8aff49641e3282f12fa7d088048f15f8033d045e
Jaime Lopez <jailop@gmail.com>
2017-05-22 23:17:43 -0600

    xml Read and Writer

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-LDLIBS = `pkg-config --libs gtk+-3.0`
-CFLAGS = -Wall -g -std=c99 -export-dynamic `pkg-config --cflags gtk+-3.0`
+LDLIBS = -L/usr/lib/libxml2 -lxml2 `pkg-config --libs gtk+-3.0`
+CFLAGS = -Wall -g -std=c99 -DDEBUG -export-dynamic -I/usr/include/libxml2 `pkg-config --cflags gtk+-3.0`
 
 all: gqda 
 
--- gqda.c
+++ gqda.c
@@ -1,6 +1,8 @@
-#include <gtk/gtk.h>
 #include <stdlib.h>
 #include <string.h>
+#include <gtk/gtk.h>
+#include <libxml/xmlwriter.h>
+#include <libxml/xmlreader.h>
 
 GtkWidget *window,
           *note_tree,
@@ -14,8 +16,127 @@ enum {
     NOTE_COLS
 };
 
-unsigned int notes_counter = 0;
+struct note_t {
+    char *label;
+    char *content;
+    char *id;
+    char *pos;
+};
 
+unsigned int notes_counter = 0;
+char *file = NULL;
+
+char *astrcpy(const char *s)
+{
+    char *ret;
+    int len = strlen(s) + 1;
+    ret = malloc(sizeof(char) * len);
+    strcpy(ret, s);
+    return ret;
+}
+
+int xml_read_note(xmlTextReaderPtr reader, GtkListStore *store)
+{
+    gchar *name;
+    gchar *content;
+    GtkTreeIter iter;
+    int id;
+    id = atoi((char *) xmlTextReaderGetAttribute(reader, BAD_CAST "id"));
+    xmlTextReaderRead(reader);
+    xmlTextReaderRead(reader);
+    name = astrcpy((char *) xmlTextReaderReadString(reader));
+    xmlTextReaderRead(reader);
+    xmlTextReaderRead(reader);
+    xmlTextReaderRead(reader);
+    content = astrcpy((char *) xmlTextReaderValue(reader));
+    gtk_list_store_append(store, &iter);
+    gtk_list_store_set(store, &iter, 
+        NOTE_NAME, name, 
+        NOTE_CONTENT, content, 
+        NOTE_ID, id, 
+        -1);
+    return 1;
+}
+
+int xml_open_file(const char *filename)
+{
+#ifdef DEBUG
+    printf("Function: xml_read_file\n");
+#endif
+    char *tagname;
+    int ret;
+    GtkListStore *store;
+    store = GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(note_tree))); 
+    gtk_list_store_clear(store);
+    xmlTextReaderPtr reader = xmlReaderForFile(filename, NULL, 0);
+    ret = xmlTextReaderRead(reader);
+    while (ret == 1) {
+        tagname = (char *) xmlTextReaderConstName(reader);
+        if (strcmp(tagname, "note") == 0 && xmlTextReaderNodeType(reader) == 1)
+            xml_read_note(reader, store);
+        else
+            printf("Tag: %s\n", tagname);
+        ret = xmlTextReaderRead(reader);
+    }
+    xmlFreeTextReader(reader);
+    return 1;
+}
+
+gboolean xml_write_each_note(GtkTreeModel *model, GtkTreePath *path, GtkTreeIter *iter, gpointer data)
+{
+#ifdef DEBUG
+    printf("Function: xml_write_each_note\n");
+#endif
+    xmlTextWriterPtr writer = (xmlTextWriterPtr) data;
+    gchar *name;
+    gchar *content;
+    int id;
+    gtk_tree_model_get(model, iter,
+            NOTE_NAME, &name,
+            NOTE_CONTENT, &content,
+            NOTE_ID, &id,
+            -1);
+    xmlTextWriterStartElement(writer, BAD_CAST "note");
+    xmlTextWriterWriteFormatAttribute(writer, BAD_CAST "id", "%d", id);
+    xmlTextWriterWriteElement(writer, BAD_CAST "name", BAD_CAST name);
+    xmlTextWriterWriteElement(writer, BAD_CAST "content", BAD_CAST content);
+    xmlTextWriterEndElement(writer);
+    return FALSE;
+}
+
+int xml_write_notes(xmlTextWriterPtr writer)
+{
+#ifdef DEBUG
+    printf("Function: xml_write_notes\n");
+#endif
+    GtkTreeModel *store = gtk_tree_view_get_model(GTK_TREE_VIEW(note_tree)); 
+    xmlTextWriterStartElement(writer, BAD_CAST "notes");
+    gtk_tree_model_foreach(store, xml_write_each_note, writer); 
+    xmlTextWriterEndElement(writer);
+    return 1;
+}
+
+int xml_write_file(const char *filename)
+{
+#ifdef DEBUG
+    printf("Function: write_file %s\n", filename);
+#endif
+    xmlTextWriterPtr writer;
+
+    writer = xmlNewTextWriterFilename(filename, 0);
+    if (writer == NULL) {
+        printf("xmlWriter failure: error creating writer\n");
+        return 0;
+    }
+
+    xmlTextWriterStartDocument(writer, NULL, "UTF-8", NULL);
+    xmlTextWriterStartElement(writer, BAD_CAST "data");
+    xml_write_notes(writer);
+    xmlTextWriterEndElement(writer);
+    xmlTextWriterEndDocument(writer);
+    xmlFreeTextWriter(writer);
+    return 1;
+}
 int main(int argc, char **argv)
 {
     gtk_init(&argc, &argv);
@@ -58,7 +179,7 @@ gboolean on_open_note(GtkWidget *widget, gpointer userdata)
         GtkListStore *note_store =     
             GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(note_tree))); 
         GtkTreeIter iter;
-        char *filename = strrchr(filepath, '/');
+        char *filename = strrchr(filepath, '/') + 1;
         gtk_list_store_append(note_store, &iter);
         gtk_list_store_set(note_store, &iter, 
                 NOTE_NAME, filename, 
@@ -98,7 +219,7 @@ gboolean on_note_tree_row_activated(GtkTreeView *tree, GtkTreePath *path,
     
     gchar *name;
     gchar *content;
-    guint *id;
+    guint id;
     gtk_tree_model_get(note_store, &iter,
             NOTE_NAME, &name,
             NOTE_CONTENT, &content,
@@ -109,5 +230,22 @@ gboolean on_note_tree_row_activated(GtkTreeView *tree, GtkTreePath *path,
     gtk_text_buffer_set_text(tb, content, strlen(content));
     free(name);
     free(content);
+    return FALSE;
+}
+
+gboolean on_project_new(GtkWidget *widget, gpointer data)
+{
+    return FALSE;
+}
+
+gboolean on_project_open(GtkWidget *widget, gpointer data)
+{
+    xml_open_file("test.gqda");
+    return FALSE;
+}
+
+gboolean on_project_save(GtkWidget *widget, gpointer data)
+{
+    xml_write_file("test.gqda");
     return FALSE;
 }
