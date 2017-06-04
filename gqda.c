ebc54ed1177982cb8f0b26e078d86768b88f9cd0
Jaime Lopez <jailop@gmail.com>
2017-06-03 20:42:24 -0600

    Search in active note

--- gqda.c
+++ gqda.c
@@ -486,7 +486,6 @@ gboolean tag_tree_view_changed(GtkWidget *widget, gpointer data)
 gboolean on_search_changed(GtkWidget *widget, gpointer data)
 {
     int found;
-    int cursor_position;
     const char *text;
     GtkTextBuffer *buffer;
     GtkTextView   *view;
@@ -496,8 +495,7 @@ gboolean on_search_changed(GtkWidget *widget, gpointer data)
         return FALSE;
     view = GTK_TEXT_VIEW(data);
     buffer = gtk_text_view_get_buffer(view);
-    g_object_get(G_OBJECT(buffer), "cursor-position", &cursor_position, NULL);
-    gtk_text_buffer_get_iter_at_offset(buffer, &start, cursor_position);
+    gtk_text_buffer_get_start_iter(buffer, &start);
     found = gtk_text_iter_forward_search(&start, text, 
             GTK_TEXT_SEARCH_CASE_INSENSITIVE,
             &match_start, &match_end, NULL);
@@ -508,12 +506,52 @@ gboolean on_search_changed(GtkWidget *widget, gpointer data)
     return FALSE;
 }
 
-gboolean on_search_keypress(GtkWidget *widget, GdkEventKey *ev, gpointer data)
+gboolean on_search_forward(GtkWidget *widget, gpointer data)
 {
-    switch (ev->keyval) {
-        case GDK_KEY_Return:
-            on_search_changed(widget, data);
-            break;
+    int found;
+    const char *text;
+    GtkTextBuffer *buffer;
+    GtkTextView *view;
+    GtkTextIter start, end, match_start, match_end;
+    text = gtk_entry_get_text(GTK_ENTRY(data));
+    if (!text)
+        return FALSE;
+    view = GTK_TEXT_VIEW(app.note_view);
+    buffer = gtk_text_view_get_buffer(view);
+    found = gtk_text_buffer_get_selection_bounds(buffer, &start, &end);
+    if (!found)
+        return FALSE;
+    found = gtk_text_iter_forward_search(&end, text,
+            GTK_TEXT_SEARCH_CASE_INSENSITIVE,
+            &match_start, &match_end, NULL);
+    if (found) {
+        gtk_text_buffer_select_range(buffer, &match_start, &match_end);
+        gtk_text_view_scroll_to_iter(view, &match_start, 0.0, FALSE, 0.0, 0.0);
+    }
+    return FALSE;
+}
+
+gboolean on_search_backward(GtkWidget *widget, gpointer data)
+{
+    int found;
+    const char *text;
+    GtkTextBuffer *buffer;
+    GtkTextView *view;
+    GtkTextIter start, end, match_start, match_end;
+    text = gtk_entry_get_text(GTK_ENTRY(data));
+    if (!text)
+        return FALSE;
+    view = GTK_TEXT_VIEW(app.note_view);
+    buffer = gtk_text_view_get_buffer(view);
+    found = gtk_text_buffer_get_selection_bounds(buffer, &start, &end);
+    if (!found)
+        return FALSE;
+    found = gtk_text_iter_backward_search(&start, text,
+            GTK_TEXT_SEARCH_CASE_INSENSITIVE,
+            &match_start, &match_end, NULL);
+    if (found) {
+        gtk_text_buffer_select_range(buffer, &match_start, &match_end);
+        gtk_text_view_scroll_to_iter(view, &match_start, 0.0, FALSE, 0.0, 0.0);
     }
     return FALSE;
 }
