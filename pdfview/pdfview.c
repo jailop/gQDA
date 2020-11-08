d96f2b369f9bdbb80fac317bf187f2b01e0aabef
Jaime Lopez <jailop@protonmail.com>
2020-11-07 22:31:47 -0500

    pdfview working

--- pdfview/pdfview.c
+++ pdfview/pdfview.c
@@ -2,7 +2,7 @@
 #include <gtk/gtk.h>
 #include <poppler/glib/poppler.h>
 
-GtkWidget *canvas = NULL;
+GtkDrawingArea *canvas = NULL;
 guint max_pages = 1;
 guint cur_page  = 0;
 PopplerDocument *doc;
@@ -11,11 +11,9 @@ PopplerPage *page;
 void slide_show()
 {
     page = poppler_document_get_page(doc, cur_page);
-    gtk_widget_queue_draw_area(canvas, 0, 0, 
-                               gtk_widget_get_allocated_width(canvas),
-                               gtk_widget_get_allocated_height(canvas));
-    printf("Page %d/%d\n", cur_page, max_pages);
-    fflush(stdout);
+    gtk_widget_queue_draw_area(GTK_WIDGET(canvas), 0, 0, 
+        gtk_widget_get_allocated_width(GTK_WIDGET(canvas)),
+        gtk_widget_get_allocated_height(GTK_WIDGET(canvas)));
 }
 
 gboolean keypress_callback(GtkWidget *widget, GdkEventKey *ev, 
@@ -56,16 +54,11 @@ gboolean forward_callback(GtkWidget *widget, gpointer data)
 
 gboolean draw_callback(GtkWidget *widget, cairo_t *cr, gpointer data)
 {
-    guint width, height;
+    guint width;
     gdouble popwidth, popheight, scale_factor;
-    
-    width = gtk_widget_get_allocated_width(canvas);
-    height = gtk_widget_get_allocated_height(canvas);
-    
+    width = gtk_widget_get_allocated_width(GTK_WIDGET(canvas));
     poppler_page_get_size(page, &popwidth, &popheight);
-    
     scale_factor = width / popwidth;
-
     cairo_scale(cr, scale_factor, scale_factor);
     poppler_page_render(page, cr);
     return FALSE;
@@ -79,24 +72,33 @@ gboolean quit_callback(GtkWidget *widget, gpointer data)
 
 int main(int argc, char **argv)
 {
-    GtkWidget *window = NULL;
+    GtkWindow *window;
     gchar *uri = NULL;
-
+    GError *error;
     gtk_init(&argc, &argv);
-    GtkBuilder *builder = gtk_builder_new_from_file("pdfview.glade");
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "Window"));
-    canvas = GTK_WIDGET(gtk_builder_get_object(builder, "DrawingArea"));
+    window = (GtkWindow*) gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size(window, 800, 600);
+    gtk_window_set_title(window, "PDF Viewer");
+    // g_signal_connect(window, "destroy", quit_callback, NULL);
+    g_signal_connect(window, "destroy", gtk_main_quit, NULL);
+    
+    canvas = (GtkDrawingArea*) gtk_drawing_area_new();
+    gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(canvas));
+    g_signal_connect((GtkWidget*) window, "key-press-event",            
+        G_CALLBACK(keypress_callback), NULL);
+    g_signal_connect(GTK_WIDGET(canvas), "draw", G_CALLBACK(draw_callback), NULL);
     
     uri = g_strconcat("file://", argv[1], NULL);
     
-    doc = poppler_document_new_from_file(uri, NULL, NULL);
+    doc = poppler_document_new_from_file(uri, NULL, &error);
+    if (!doc) {
+        fprintf(stderr, "Document could not be openned\n%s\n", error->message);
+        exit(EXIT_FAILURE);
+    }
     max_pages = poppler_document_get_n_pages(doc);
     page = poppler_document_get_page(doc, cur_page);
-
-    gtk_builder_connect_signals(builder, NULL);
-    g_object_unref(G_OBJECT(builder));
     
-    gtk_widget_show_all(window);
+    gtk_widget_show_all(GTK_WIDGET(window));
     gtk_main();
     return 0;
 }
